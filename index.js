5357e12fccf7c9948970295b5ce8a238d0340ca5
hiraBiswas <hbhirabiswas@gmail.com>
2023-10-20 14:30:54 +0600

    product data has been stored in db

--- index.js
+++ index.js
@@ -38,54 +38,6 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/product/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await productCollection.findOne(query);
-      res.send(result);
-  })
-
-
-    app.put('/product/:id', async (req, res) => {
-      const id = req.params.id;
-      const filter= { _id: new ObjectId(id) }
-      const options = { upsert: true };
-      const updateProduct = req.body;
-
-      const product= {
-          $set: {
-              image: updateProduct.image,
-              name: updateProduct.name,
-              brandName: updateProduct.brandName,
-              price: updateProduct.price,
-              type: updateProduct.type,
-              rating: updateProduct.rating,
-              description: updateProduct.description
-          }
-      }
-
-      const result = await productCollection.updateOne(filter, product, options);
-      res.send(result);
-  })
-
-
-  app.post('/product', async (req, res) => {
-    const newProduct= req.body;
-    console.log(newProduct);
-    const result = await productCollection.insertOne(newProduct)
-    res.send(result)
-  })
-    
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
-
 
 app.get('/', (req, res) => {
     res.send('cosmetic store server is running')
